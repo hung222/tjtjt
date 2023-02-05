3a075306adce8218351f9527e16e0509aa6b55c1
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-05 16:15:19 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'giác hơi 100 hủ'
-@app.route('/trangthu2.html/<name>')
-def trangthu2():
-    return render_template('trangthu2.html', name=name)
-if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+del qhtdvovigaigh('/')
+     return 'qhtddcktkkmqaqlg'
+
