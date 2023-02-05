c49cabc2a46b4642d854cf7911e9e788338c1a6e
hung222 <90371118+hung222@users.noreply.github.com>
2023-02-05 09:59:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ def hello_world():
     return 'giác hơi 100 hủ'
 @app.route('/trangthu2.html/<name>')
 def trangthu2():
-      return render_template('trangthu2.html', name=name)
+    return render_template('trangthu2.html', name=name)
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
